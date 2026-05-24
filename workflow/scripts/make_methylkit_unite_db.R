23b2b01667a43c514218b39f30ea1d21ce5f3923
tmweiskittel <93729347+tmweiskittel@users.noreply.github.com>
2026-05-24 17:39:10 -0500

    Create make_methylkit_unite_db.R

---
+++ workflow/scripts/make_methylkit_unite_db.R
@@ -0,0 +1,47 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages({
+  library(methylKit)
+  library(optparse)
+})
+
+opts <- parse_args(OptionParser(option_list = list(
+  make_option("--lib_db_list", type = "character"),
+  make_option("--lib_id_list", type = "character"),
+  make_option("--treatment_list", type = "character"),
+  make_option("--cores", type = "integer", default = 1),
+  make_option("--out_dir", type = "character"),
+  make_option("--suffix", type = "character"),
+  make_option("--assembly", type = "character", default = "hg38"),
+  make_option("--mincov", type = "integer", default = 5),
+  make_option("--min_per_group", type = "integer", default = 2),
+  make_option("--chunk_size", type = "integer", default = 1000000)
+)))
+
+dir.create(opts$out_dir, recursive = TRUE, showWarnings = FALSE)
+
+files <- strsplit(opts$lib_db_list, " ")[[1]]
+ids <- strsplit(opts$lib_id_list, " ")[[1]]
+tx <- as.integer(strsplit(opts$treatment_list, " ")[[1]])
+
+meth <- methRead(
+  location = as.list(files),
+  sample.id = as.list(ids),
+  assembly = opts$assembly,
+  treatment = tx,
+  context = "CpG",
+  pipeline = "methylKit",
+  mincov = opts$mincov,
+  dbtype = "tabix",
+  dbdir = opts$out_dir
+)
+
+unite(
+  meth,
+  destrand = FALSE,
+  min.per.group = opts$min_per_group,
+  mc.cores = opts$cores,
+  save.db = TRUE,
+  dbdir = opts$out_dir,
+  suffix = opts$suffix
+)
