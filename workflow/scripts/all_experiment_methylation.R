38b3b8b054ce2a4b9d6a0c3d0ea7eaf282f1b0a0
tmweiskittel <93729347+tmweiskittel@users.noreply.github.com>
2026-05-24 17:41:13 -0500

    Create all_experiment_methylation.R

---
+++ workflow/scripts/all_experiment_methylation.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages({
+  library(methylKit)
+  library(optparse)
+  library(data.table)
+})
+
+opts <- parse_args(OptionParser(option_list = list(
+  make_option("--db_file", type = "character"),
+  make_option("--out_file", type = "character")
+)))
+
+dir.create(dirname(opts$out_file), recursive = TRUE, showWarnings = FALSE)
+
+mbase <- readMethylBaseDB(opts$db_file)
+df <- as.data.table(getData(mbase))
+
+coverage_cols <- grep("\\.coverage$", names(df), value = TRUE)
+numCs_cols <- sub("\\.coverage$", ".numCs", coverage_cols)
+
+out <- df[, .(chr, start, end, strand)]
+
+for (i in seq_along(coverage_cols)) {
+  sample <- sub("\\.coverage$", "", coverage_cols[i])
+  out[[sample]] <- 100 * df[[numCs_cols[i]]] / df[[coverage_cols[i]]]
+}
+
+fwrite(out, opts$out_file, sep = "\t", quote = FALSE, na = "NA")
