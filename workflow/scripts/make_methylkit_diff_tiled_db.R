7fa560dd78f6359833ce222935cec9118536dff2
tmweiskittel <93729347+tmweiskittel@users.noreply.github.com>
2026-05-27 20:03:25 -0500

    Update make_methylkit_diff_tiled_db.R

--- workflow/scripts/make_methylkit_diff_tiled_db.R
+++ workflow/scripts/make_methylkit_diff_tiled_db.R
@@ -33,7 +33,6 @@ meth <- methRead(
   context = "CpG",
   mincov = opts$mincov,
   dbtype = "tabix",
-  pipeline="bismarkCoverage",
   dbdir = opts$out_dir
 )
 
@@ -55,10 +54,16 @@ mbase <- unite(
   suffix = paste0(opts$suffix, ".tiled")
 )
 
-calculateDiffMeth(
+mdiff= calculateDiffMeth(
   mbase,
   mc.cores = opts$cores,
   save.db = FALSE,
   dbdir = opts$out_dir,
   suffix = paste0(opts$suffix, ".tiled")
 )
+
+mbase_out <- file.path(opts$out_dir, paste0("methylBase_", opts$suffix, ".tiled.txt"))
+mdiff_out <- file.path(opts$out_dir, paste0("methylDiff_", opts$suffix, ".tiled.txt"))
+
+save.db(mbase, file = mbase_out, dbtype = "tabix", tabix = TRUE)
+save.db(mdiff, file = mdiff_out, dbtype = "tabix", tabix = TRUE)
